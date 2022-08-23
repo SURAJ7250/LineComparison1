20169724a6b12701a20185ad14ab1c0caf7af40d
surajbridgelabz <surajnsinha@gmail.com>
2022-08-23 21:48:13 +0530

    [suraj] Add: compare 2 lengths

--- LineComparisonProblem/LineComparisonProblem/Program.cs
+++ LineComparisonProblem/LineComparisonProblem/Program.cs
@@ -12,12 +12,12 @@ namespace LineComparisonProblem
             Comparision LineComparisonProblem = new Comparision(1, 2, 3, 4);
             double line2 = LineComparisonProblem.CalculateLength();
             Console.WriteLine("Length of second line" + line2);
-            if (line1 == line2)
-            {
-                Console.WriteLine("Line 1 and Line 2 are equal");
-            }
+            if (line1.CompareTo(line2) == 0)
+                Console.WriteLine("Both lines are same");
+            if (line1.CompareTo(line2) > 0)
+                Console.WriteLine("Line 1 is greater than line 2");
             else
-                Console.WriteLine("Not equal");
+                Console.WriteLine("Line 2 is greater than line 1");
         }
     }
 }
