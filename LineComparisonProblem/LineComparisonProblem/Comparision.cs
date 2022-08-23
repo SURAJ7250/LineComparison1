1112e04bd2c2fd7b4a515d8836ce7b17e9fc4095
surajbridgelabz <surajnsinha@gmail.com>
2022-08-23 22:09:32 +0530

    [suraj] Add: check equality

--- LineComparisonProblem/LineComparisonProblem/Comparision.cs
+++ LineComparisonProblem/LineComparisonProblem/Comparision.cs
@@ -19,7 +19,7 @@ namespace LineComparisonProblem
         public void CalculateLength()
         {
             double lineLength = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            return CalculateLength();
+            return lineLength;
 
         }
     }
